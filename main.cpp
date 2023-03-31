fd901a7d05bc7e958a25957380cfdba88aec4b5b
firas <firas.gabetni@etudiant-enit.utm.tn>
2023-03-31 17:08:29 +0200

    added dark theme

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "CBR.h"
 #include <QtWidgets/QApplication>
+#include <QtCore/QFile>
+#include <QtCore/QString>
+#include <QtCore/QLatin1String>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     CBR w;
+	//open qss file
+    QFile file("Combinear.qss");
+    file.open(QFile::ReadOnly);
+
+    // Read qss file contents into a QString using QLatin1String
+    QString styleSheet{ QLatin1String(file.readAll()) };
+
+	//setup stylesheet
+	a.setStyleSheet(styleSheet);
     w.show();
     return a.exec();
 }
